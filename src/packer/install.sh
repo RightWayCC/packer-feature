14062dbbc087d1ab9de892807e2ae4e25bca89d6
Logan <10239411+Loag@users.noreply.github.com>
2023-04-29 16:11:28 -0400

    fix sudo use

--- src/packer/install.sh
+++ src/packer/install.sh
@@ -3,9 +3,11 @@ set -e
 
 # check for wget
 which wget > /dev/null || (apt update && apt install wget -y -qq)
+# check for pgp
+which gpg > /dev/null || (apt update && apt install gpg -y -qq)
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt update && sudo apt install packer
+apt update && apt install packer
