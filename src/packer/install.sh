1468afa75da358056d3b9cc724402e1b8b7401c9
Logan <10239411+Loag@users.noreply.github.com>
2023-04-29 16:08:13 -0400

    added packer

---
+++ src/packer/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# check for wget
+which wget > /dev/null || (apt update && apt install wget -y -qq)
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update && sudo apt install packer
