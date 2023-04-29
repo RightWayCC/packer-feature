42687b253032ade9333d5a25bda6f9e0f2e9e135
Logan <10239411+Loag@users.noreply.github.com>
2023-04-29 16:14:12 -0400

    install lsb release

--- src/packer/install.sh
+++ src/packer/install.sh
@@ -6,6 +6,8 @@ which wget > /dev/null || (apt update && apt install wget -y -qq)
 # check for pgp
 which gpg > /dev/null || (apt update && apt install gpg -y -qq)
 
+which lsb_release > /dev/null || (apt update && apt install lsb_release -y -qq)
+
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
